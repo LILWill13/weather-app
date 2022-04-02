4c2129b95199d90d7ed8715b3cc102b1ac6bb1d9
noah gaston <noahsfuture18@gmail.com>
2022-04-02 10:17:57 -0400

    testing js

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,21 @@
 
 
-var cityInput = $('#cityInput').value
+
 
 
 var cityBtn = $('#cityBtn')
+cityBtn.click(zb)
 
+function zb(){
+    var cityInput = $('#cityInput').val()
+    
 
-var apiKey = '5dd607f72025869fc95a703b9eda329e';
+    var apiKey = '5dd607f72025869fc95a703b9eda329e';
 
- var url ='api.openweathermap.org/data/2.5/weather?q='+ cityInput +'&appid='+ apiKey;
- console.log(url)
+ 
+    var url ='api.openweathermap.org/geo/1.0/direct?q='+ cityInput+'&appid='+ apiKey;
+
+console.log(url)
+
+   fetch(url);
+}
